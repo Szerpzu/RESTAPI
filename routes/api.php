24e9c0d631e0221e8462528930b7acc494ece981
Szerpzu <tomekrbk@wp.pl>
2023-05-31 22:34:57 +0200

    web aktu

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,18 @@
 <?php
 
-use App\Http\Controllers\PeopleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/people', [PeopleController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/people/{id}', [PeopleController::class, 'show']);
-
-Route::put('/people/{id}', [PeopleController::class, 'update']);
-
-Route::delete('/people/{id}', [PeopleController::class, 'destroy']);
-
-Route::post('/people', [PeopleController::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
