a010f4e08a604c8ffde6c47a23bbcedb5d4a1f6b
Szerpzu <tomekrbk@wp.pl>
2023-05-31 20:57:18 +0200

    naprawa routingu

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PeopleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/people', [PeopleController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/people/{id}', [PeopleController::class, 'show']);
+
+Route::put('/people/{id}', [PeopleController::class, 'update']);
+
+Route::delete('/people/{id}', [PeopleController::class, 'destroy']);
+
+Route::post('/people', [PeopleController::class, 'store']);
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\PeopleController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/people', [PeopleController::class, 'index']);
-
-Route::get('/people/{id}', [PeopleController::class, 'show']);
-
-Route::put('/people/{id}', [PeopleController::class, 'update']);
-
-Route::delete('/people/{id}', [PeopleController::class, 'destroy']);
-
-Route::post('/people', [PeopleController::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
