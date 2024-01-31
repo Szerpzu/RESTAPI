e8431f66b5d6e78664307acc05934eebd028a216
Szerpzu <tomekrbk@wp.pl>
2024-01-31 20:41:16 +0100

    commit	modified:   app/Http/Middleware/VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/http://127.0.0.1:8000/people/*'
-        '/http://127.0.0.1:8000/*
+        '/http://127.0.0.1:8000/people/*',
+        '/http://127.0.0.1:8000/people',
     
 
     ];
